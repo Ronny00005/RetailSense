f7042cf0522e8938cd1be642553381c29eaa77aa
Ronny00005 <ronnypatel2005@gmail.com>
2026-02-09 10:57:43 +0530

    Fixed some ui changes

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -34,7 +34,11 @@ function showSection(sectionId) {
     // Load charts only when needed
     if (sectionId === "category") {
         loadCategoryCharts();
-    
+     if (sectionId === "inventory") {
+        setTimeout(() => {
+            loadInventory();
+        }, 200);
+    }
 }
 if (sectionId === "future") {
     fetch("/api/forecast-chart")
